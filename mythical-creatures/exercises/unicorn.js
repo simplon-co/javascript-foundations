520e8fd03568b44c2e40caf914630b143679ec7b
cheroliv@laposte.net <1975Imrandeh>
2020-01-09 15:35:27 +0100

    ____

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,6 +1,16 @@
 class Unicorn {
-  constructor() {
+  name
+  constructor(name) {
+    this.name = name;
   }
 }
 
 module.exports = Unicorn;
+// 1. Read the test
+// 2. Ask yourself if its going to fail and why
+// 3. Run the test
+// 4. make it pass
+
+let unicorn={
+  name:'uni'
+}
